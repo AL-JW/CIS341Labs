c641f1e7bf5cfd1b9f7410209f81895e7453dd16
Alex Woynich <awoyn002@uwsp.edu>
2023-10-01 19:17:05 -0500

    Add project files.

---
+++ CIS341-lab3/Pages/Contact.cshtml.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CIS341_lab3.Pages
+{
+    public class ContactFormData
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+
+
+    public class ContactModel : PageModel
+    {
+        [BindProperty]
+        public ContactFormData FormData { get; set; } = new ContactFormData();
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                Console.WriteLine($"Name: {FormData.Name}, Email: {FormData.Email}, Message: {FormData.Message}");
+
+                return RedirectToPage("Index");
+            }
+
+            return Page();
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ CIS341-lab3/Program.cs
@@ -0,0 +1,48 @@
+using System.Diagnostics;
+
+namespace CIS341_lab3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.Use(async (context, next) =>
+            {
+                //This writes the current time and request url 
+                Debug.WriteLine($"Time: {DateTime.Now}, URL: {context.Request.Path}");
+
+
+                //go to the next delegate in the pipeline
+
+                await next.Invoke();
+            });
+
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
