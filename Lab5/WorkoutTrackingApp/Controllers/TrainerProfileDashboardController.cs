898f9a19a0d2112199285403d9f0eae5e9b66cb0
Alex Woynich <awoyn002@uwsp.edu>
2023-10-22 15:36:53 -0500

    Lab5 Current Progress.

---
+++ Lab5/WorkoutTrackingApp/Controllers/TrainerProfileDashboardController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WorkoutTrackingApp.Controllers
+{
+    public class TrainerProfileDashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+        public IActionResult ManageWorkouts()
+        {
+            return View();
+        }
+
+        public IActionResult ManageExercises()
+        {
+
+            return View();
+        }
+
+        public IActionResult Subscribers()
+        {
+            return View();
+        }
+
+        public IActionResult PopularWorkouts()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Lab5/WorkoutTrackingApp/Controllers/UserProfileDashboardController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WorkoutTrackingApp.Controllers
+{
+    public class UserProfileDashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Profile() 
+        {
+
+            return View();  
+            
+        }
+
+        public IActionResult Subscription()
+        {
+            return View();
+        }
+
+        public IActionResult Settings()
+        {
+            return View();
+        }
+
+
+        public IActionResult Picture()
+        {
+            return View();
+        }
+
+        public IActionResult LatestWorkout() 
+        {
+            return View();
+        }
+    }
+}
