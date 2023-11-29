6181a0c758920ee755a070affa09bfb15f218fb8
Alex Woynich <awoyn002@uwsp.edu>
2023-11-28 22:55:11 -0600

    Updates to the UI added buttons for create and edit, also updated the nav bar a little bit, still need to figure out whats going on with the database

---
+++ Fitness_App_1128_Progress/WorkoutTrackingApp/Models/TrackedWorkout.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WorkoutTrackingApp.Models
+{
+    public class TrackedWorkout
+    {
+        [Key]
+        public int TrackedWorkoutId { get; set; } //The primary key for tracked workouts
+
+        //public int AccountId { get; set; }
+
+        //Foreign key property
+       // [ForeignKey("AccountId")]
+        //public Account Account { get; set; }
+
+        //The navigation property needed with the foreign key
+       // public virtual Account Account { get; set; }
+
+        [Required]
+        public int WorkoutId { get; set; }
+
+        [Required]
+        [Display(Name = "Date Completed")]
+        [DataType(DataType.Date)]
+        public DateTime DateCompleted { get; set; }
+        
+    }
+}
---
+++ Fitness_App_1128_Progress/WorkoutTrackingApp/Models/Workout.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WorkoutTrackingApp.Models
+{
+    public class Workout
+    {
+
+       // public int AccountId { get; set; }
+
+        //Navigation property
+        //[ForeignKey("AccountId")]
+        //public virtual Account Account { get; set; }
+
+        [Key]
+        public int WorkoutId { get; set; } //Primary key
+
+        
+
+        [Required]
+        [Display(Name = "Workout Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Author")]
+        public string Author { get; set; }
+
+        //[Display(Name = "Select Exercises")]
+        //public int[] ExerciseIds { get; set; }
+
+        public virtual ICollection<Exercise> Exercises { get; set; }
+        
+       //Navigation property for join table WorkoutExercise
+
+        //Probably do not need this along with the Exercies
+        //public virtual ICollection<WorkoutExercise> WorkoutExercises { get; set; }
+
+
+    }
+}
