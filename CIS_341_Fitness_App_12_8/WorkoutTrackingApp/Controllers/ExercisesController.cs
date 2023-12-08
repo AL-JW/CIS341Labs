8f4d724ed7f2484d94d2480501c26e12653d5e88
Alex Woynich <awoyn002@uwsp.edu>
2023-12-08 00:10:37 -0600

    Updates to the fitness app: nav bar, bootstrap css, workouts, exercises, home page, trainer dashboard started for crud of workouts and exercises

---
+++ CIS_341_Fitness_App_12_8/WorkoutTrackingApp/Controllers/ExercisesController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WorkoutTrackingApp.Data;
+using WorkoutTrackingApp.Models;
+using WorkoutTrackingApp.ViewModels;
+
+namespace WorkoutTrackingApp.Controllers
+{
+    
+
+    public class ExercisesController : Controller
+    {
+
+        private readonly WorkoutTrackingAppContext _context; // Defining the database context
+
+        public ExercisesController(WorkoutTrackingAppContext context)
+        {
+            _context = context;
+        }
+
+        // [AllowAnonymous]
+        public IActionResult Index()
+        {
+            var exercises = _context.Exercises
+                                  .Select(e => new ExerciseViewModel
+                                  {
+                                      Id = e.ExerciseId,
+                                      Name = e.Name,
+                                      Description = e.Description,
+                                      // ...other properties...
+                                  })
+                                  .ToList();
+
+            return View(exercises);
+        }
+
+       // [AllowAnonymous]
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        //[Authorize]
+        [HttpGet]
+        public IActionResult Create()
+        {
+            //return View(new ExerciseViewModel());
+            return View("ManageExercises", new ExerciseViewModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ExerciseViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var exercise = new Exercise // Assuming 'Exercise' is your model class
+                {
+                    Name = model.Name,
+                    Description = model.Description,
+                    Intensity = model.Intensity,
+                    // ...set other properties from the model...
+                };
+
+                _context.Exercises.Add(exercise);
+                _context.SaveChanges();
+
+                // Redirect to the index action (or wherever appropriate)
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+       // [Authorize]
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Delete() 
+        {
+            return View();
+        }
+    }
+}
---
+++ CIS_341_Fitness_App_12_8/WorkoutTrackingApp/ViewModels/ExerciseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using WorkoutTrackingApp.Models;
+
+namespace WorkoutTrackingApp.ViewModels
+{
+    public class ExerciseViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+        public string TargetMuscleGroup { get; set; }
+        
+        public ExerciseIntensity Intensity { get; set; }
+        public string EquipmentRequired { get; set; }
+        public string Difficulty { get; set; }       
+    }
+}
