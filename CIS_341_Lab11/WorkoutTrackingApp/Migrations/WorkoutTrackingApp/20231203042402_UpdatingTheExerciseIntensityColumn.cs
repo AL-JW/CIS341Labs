744af6f7d7c37f4910c9fc8d6a1d353d77d81182
Alex Woynich <awoyn002@uwsp.edu>
2023-12-11 19:00:17 -0600

    Lab 11, I know this is probably not even close to being correct, but I attempted it.

---
+++ CIS_341_Lab11/WorkoutTrackingApp/Migrations/WorkoutTrackingApp/20231203042402_UpdatingTheExerciseIntensityColumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkoutTrackingApp.Migrations.WorkoutTrackingApp
+{
+    /// <inheritdoc />
+    public partial class UpdatingTheExerciseIntensityColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Length",
+                table: "Exercises",
+                newName: "Intensity");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Intensity",
+                table: "Exercises",
+                newName: "Length");
+        }
+    }
+}
---
+++ CIS_341_Lab11/WorkoutTrackingApp/ViewModels/TrainerDashboardViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace WorkoutTrackingApp.ViewModels
+{
+    public class TrainerDashboardViewModel
+    {
+        // Basic trainer profile information
+        public string TrainerName { get; set; }
+        public string Email { get; set; }
+
+        // Trainer's messages to and from users
+        public IEnumerable<MessageViewModel> Messages { get; set; }
+
+        // Manage workouts - This can include summaries or a list of workouts the trainer has created
+        public IEnumerable<WorkoutViewModel> ManageWorkouts { get; set; }
+
+        // Manage exercises - Similar to workouts, not using though 
+        public IEnumerable<ExerciseViewModel> ManageExercises { get; set; }
+
+        //Subscriber list - A list of all subscribers
+        public IEnumerable<SubscriberViewModel> Subscribers { get; set; }
+
+        // Popular workouts - A list of the trainer's popular workouts
+        public IEnumerable<WorkoutViewModel> PopularWorkouts { get; set; }
+
+    }
+}
